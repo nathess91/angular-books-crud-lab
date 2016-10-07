954739792078e02b3acbe9242e21feda7049edd5
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-10-07 14:52:11 -0700

    removes nodemodules from commit history

--- public/scripts/controllers/BooksIndexController.js
+++ public/scripts/controllers/BooksIndexController.js
@@ -13,6 +13,7 @@ function BooksIndexController($http) {
     releaseDate: ''
   };
 
+  // GET ALL BOOKS
   $http({
     method: 'GET',
     url: '/api/books'
@@ -21,4 +22,18 @@ function BooksIndexController($http) {
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
+
+  // CREATE A BOOK
+  vm.createBook = function() {
+    $http({
+      method: 'POST',
+      url: '/api/books',
+      data: vm.newBook,
+    }).then(function successCallback(response) {
+      vm.books.push(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    })
+  }
+
 }
